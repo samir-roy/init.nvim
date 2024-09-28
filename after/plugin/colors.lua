97d85144cc8387b28d44b4dfecbf3b60bec3818d
Samir Roy <rsamir@gmail.com>
2024-09-28 12:44:13 +0530

    v0.2.6

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,7 @@
 -- don't use bold and italic in the color scheme
--- must be set before color scheme
+-- must be set before the color scheme
 vim.g.shinjuku_bold = false
 vim.g.shinjuku_italic = false
-vim.g.shinjuku_cursorline_transparent = true;
 
 -- set the color scheme
 vim.cmd.colorscheme('shinjuku')
@@ -15,7 +14,3 @@ vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
-
--- use black background on cursor line
-vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#121212', fg = 'none' })
-vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = '#121212', fg = 'none' })
